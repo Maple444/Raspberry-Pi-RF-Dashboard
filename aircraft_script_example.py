0045c6d9541bca621e51b778069f7d36520c3945
Maple444 <134165739+Maple444@users.noreply.github.com>
2026-04-19 19:53:58 -0400

    Added commented-out banner

--- aircraft_script_example.py
+++ aircraft_script_example.py
@@ -4,17 +4,34 @@ import time
 aircraftsFile = "aircraft_example_file.json"
 count = 0
 
+### Note: Banner code is commented out since this version does not have the os module implemented currently
+# banner = r"""
+#   ______   _______   _______          ______   __                                          ______     __            ________                             __                           
+#  /      \ |       \ |       \        /      \ |  \                                        /      \   |  \          |        \                           |  \                          
+# |  $$$$$$\| $$$$$$$\| $$$$$$$\      |  $$$$$$\ \$$  ______    _______   ______   ______  |  $$$$$$\ _| $$_          \$$$$$$$$______   ______    _______ | $$   __   ______    ______  
+# | $$___\$$| $$  | $$| $$__| $$      | $$__| $$|  \ /      \  /       \ /      \ |      \ | $$_  \$$|   $$ \           | $$  /      \ |      \  /       \| $$  /  \ /      \  /      \ 
+#  \$$    \ | $$  | $$| $$    $$      | $$    $$| $$|  $$$$$$\|  $$$$$$$|  $$$$$$\ \$$$$$$\| $$ \     \$$$$$$           | $$ |  $$$$$$\ \$$$$$$\|  $$$$$$$| $$_/  $$|  $$$$$$\|  $$$$$$\
+#  _\$$$$$$\| $$  | $$| $$$$$$$\      | $$$$$$$$| $$| $$   \$$| $$      | $$   \$$/      $$| $$$$      | $$ __          | $$ | $$   \$$/      $$| $$      | $$   $$ | $$    $$| $$   \$$
+# |  \__| $$| $$__/ $$| $$  | $$      | $$  | $$| $$| $$      | $$_____ | $$     |  $$$$$$$| $$        | $$|  \         | $$ | $$     |  $$$$$$$| $$_____ | $$$$$$\ | $$$$$$$$| $$      
+#  \$$    $$| $$    $$| $$  | $$      | $$  | $$| $$| $$       \$$     \| $$      \$$    $$| $$         \$$  $$         | $$ | $$      \$$    $$ \$$     \| $$  \$$\ \$$     \| $$      
+#   \$$$$$$  \$$$$$$$  \$$   \$$       \$$   \$$ \$$ \$$        \$$$$$$$ \$$       \$$$$$$$ \$$          \$$$$           \$$  \$$       \$$$$$$$  \$$$$$$$ \$$   \$$  \$$$$$$$ \$$                                                                                                                                                                                     
+                                                                                                                                                                                
+# """
+
+
 while True:
     try:
         with open(aircraftsFile, "r") as file:
             data = json.load(file)
             # print(data['aircraft'])
 
+            # print(banner)
+
             print("Number of aircraft: " + str(len(data['aircraft'])))
 
             for plane in data['aircraft']:
                 # print(plane['flight'] + ' at ' + str(plane["alt_baro"]) + " ft")
-                print(f"{plane.get('flight')} at {str(plane.get('alt_baro'))} ft")
+                print(f"Aircraft {plane.get('flight')} is at {str(plane.get('alt_baro'))} ft.")
 
     except FileNotFoundError:
         print("The airplane data file could not be found.")
